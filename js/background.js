0694ff390cbba4b3baf6d2f0ba3754f66a7a0a07
kminjoo9093 <kminjoo9093@naver.com>
2025-03-09 16:29:56 +0900

    bg 수정

--- js/background.js
+++ js/background.js
@@ -1,9 +1,6 @@
 const images = ["bg_1.jpg", "bg_2.jpg", "bg_3.jpg", "bg_4.jpg"];
 
-function changeBg() {
-  const bgImg = document.querySelector('#bg-img');
-  const chosenImg = images[Math.floor(Math.random() * images.length)];
-  
-  bgImg.src = `./images/${chosenImg}`;
-}
-changeBg();
+const bgImg = document.querySelector("#bg-img");
+const chosenImg = images[Math.floor(Math.random() * images.length)];
+
+bgImg.src = `./images/${chosenImg}`;
